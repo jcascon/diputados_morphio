578f941218a7b08c4ca18a2f79d672b96b8086c0
jcascon <jcascon@abc.es>
2015-04-15 13:10:06 +0200

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -2,18 +2,21 @@
 # including some code snippets below that you should find helpful
 
 require 'scraperwiki'
-# require 'mechanize'
+require 'mechanize'
 #
-# agent = Mechanize.new
+agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+page = agent.get(" http://www.congreso.es/portal/page/portal/Congreso/Congreso/Diputados?_piref73_1333056_73_1333049_1333049.next_page=/wc/menuAbecedarioInicio&tipoBusqueda=completo&idLegislatura=10")
+page.links.each do |link|
+  puts link.text
+end
 #
 # # Find somehing on the page using css selectors
 # p page.at('div.content')
 #
 # # Write out to the sqlite database using scraperwiki library
-ScraperWiki.save_sqlite(["name"], {"name" => "juan", "occupation" => "CDO ABC.es"})
+#ScraperWiki.save_sqlite(["name"], {"name" => "juan", "occupation" => "CDO ABC.es"})
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
@@ -22,4 +25,4 @@ ScraperWiki.save_sqlite(["name"], {"name" => "juan", "occupation" => "CDO ABC.es
 # on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
 # is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
 # has at least a table called data.
-puts 'hola mundo'
+#puts 'hola mundo'
