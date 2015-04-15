8df64cf31a83eff482a4ea8b41ac37171d27f6ce
jcascon <jcascon@abc.es>
2015-04-15 12:59:18 +0200

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,7 @@
 # This is a template for a Ruby scraper on Morph (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
+require 'scraperwiki'
 # require 'mechanize'
 #
 # agent = Mechanize.new
@@ -13,7 +13,7 @@
 # p page.at('div.content')
 #
 # # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+ScraperWiki.save_sqlite(["name"], {"name" => "juan", "occupation" => "CDO ABC.es"})
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
